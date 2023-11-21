eca11b9e142ace3505c051abd757f2c1fe9f6357
Andrew Tom <toma681@berkeley.edu>
2023-11-21 10:25:12 +0000

    Update county test and add event test

--- spec/models/county_spec.rb
+++ spec/models/county_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 RSpec.describe County, type: :model do
   describe 'std_fips_code' do
     it 'returns FIPS code with leading zeros' do
-      state = State.create!(name: 'Test State')
-      county = County.create!(state: state, fips_code: 1)
+      state = State.create!(name: 'Test State', symbol: 'TS', fips_code: 'YourFipsCode', is_territory: false, lat_min: 0, lat_max: 1,
+      long_min: 0, long_max: 1)
+      county = County.create!(state: state, fips_code: 1, name: "Test State", fips_class: "")
 
       expect(county.std_fips_code).to eq('001')
     end
